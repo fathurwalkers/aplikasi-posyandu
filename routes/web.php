a3354560c7ab44d1480cb3d5d157bb4b2f68e219
Fathur Walkers <fathurwalkers44@gmail.com>
2022-03-10 01:16:30 -0800

    update

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,13 @@ use App\Http\Controllers\BackController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ClientController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => '/client'], function () {
+    Route::get('/', [ClientController::class, 'index'])->name('client-home');
+    Route::get('/login', [BackController::class, 'login_client'])->name('login-client');
+    Route::get('/register', [BackController::class, 'register_client'])->name('register-client');
+});
+
+Route::group(['prefix' => '/'], function () {
+    Route::get('/', [AdminController::class, 'index'])->name('admin-home');
+    Route::get('/login-admin', [BackController::class, 'login_admin'])->name('login-admin');
 });
