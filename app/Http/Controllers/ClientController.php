4d85fb8086682feb9a1b1367a5fbf96711f424b7
Fathur Walkers <fathurwalkers44@gmail.com>
2022-03-16 09:11:17 -0700

    update

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use App\Models\Login;
+use App\Models\Data;
 
 class ClientController extends Controller
 {
