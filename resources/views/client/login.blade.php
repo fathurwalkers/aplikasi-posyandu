89a590e9bbd9f0a899d2c39668aba4a02752af01
Fathur Walkers <fathurwalkers44@gmail.com>
2022-03-16 09:42:33 -0700

    update

---
+++ resources/views/client/login.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <link rel="stylesheet" href="{{ asset('tampilan') }}/style/login.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="{{ asset('tampilan') }}/style/all.min.css">
+
+    <title>Halaman Login</title>
+  </head>
+  <body>
+    <!-- login -->
+    <section id="logina" class="logina">
+      <div class="col-12 login text-white py-2 fixed-top shadow-sm header">
+        <!-- <a href="login.html" class="mt-1 me-2 icon"><i class="bi bi-arrow-left"></i></a> -->
+        <h5 class="mx-auto col-12 pb-0 mt-1 text-center">LOGIN</h5>
+      </div>
+      <div class="container-fluid pt-5">
+        <div class="row mt-4">
+          <div class="col-12 d-flex justify-content-center">
+            <!-- <h2 class="display-1 fw-bold title text-lg-start text-center mb-4 pb-1 pt-2">E-Gizi</h2> -->
+            <img class="gambar img-thumbnail rounded-circle border-3" src="{{ asset('tampilan') }}/img/healthy-food.png" alt="gambar" />
+          </div>
+          <div class="col-12 mt-5 pt-5">
+            <form class="position-relative">
+              <div class="mb-3">
+                <input type="text" class="form-control bg-transparent border-0 border-bottom rounded-0 border-dark shadow-none ps-0 input" id="login_username" required autocomplete="off" name="login_username"/>
+                <label for="login_username" class="email">Email</label>
+              </div>
+              <div class="mt-3">
+                <input type="password" class="form-control bg-transparent border-0 border-bottom rounded-0 border-dark shadow-none ps-0" id="login_password" required name="login_password"/>
+                <label for="login_password" class="password">Password</label>
+                <span class="mata" onclick="showPass()">
+                  <i class="bi bi-eye-fill" id="hide1"></i>
+                  <i class="bi bi-eye-slash-fill" id="hide2"></i>
+                </span>
+              </div>
+              <button type="submit" class="btn tombol text-white col-12 mt-4 rounded-pill">Simpan</button>
+            </form>
+          </div>
+          {{-- <div class="col-12 text-center mt-2">
+            <a href="lupa-password.html" class="text-dark forget">Lupa Password?</a>
+          </div>
+          <div class="col-12 text-center mt-3 d-flex justify-content-center">
+            <hr />
+            <p class="px-2">atau</p>
+            <hr />
+          </div> --}}
+          <div class="col-12 text-center daftar mt-2">
+            <p>Belum punya akun ? Daftar <a href="registrasi.html">disini</a></p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- akhir login -->
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="{{ asset('tampilan') }}/js/index.js"></script>
+    {{-- <script src="{{ asset('tampilan') }}/js/lihat.js"></script> --}}
+  </body>
+</html>
