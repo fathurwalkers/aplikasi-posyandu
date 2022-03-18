28e09f7236cb3a8baf75ba729dbb6b43e16e372a
Fathur Walkers <fathurwalkers44@gmail.com>
2022-03-18 07:00:57 -0700

    update

--- database/migrations/2022_03_10_071005_create_data_table.php
+++ database/migrations/2022_03_10_071005_create_data_table.php
@@ -10,6 +10,14 @@ class CreateDataTable extends Migration
     {
         Schema::create('data_pengguna', function (Blueprint $table) {
             $table->id();
+            $table->string('data_foto')->nullable();
+            $table->string('data_nama_lengkap')->nullable();
+            $table->string('data_alamat_lengkap')->nullable();
+            $table->string('data_jenis_kelamin')->nullable();
+            $table->integer('data_usia_ukur')->nullable(); // BULAN
+            $table->dateTime('data_tanggal_lahir')->nullable();
+            $table->float('data_berat')->nullable();
+            $table->float('data_tinggi')->nullable();
             $table->timestamps();
         });
     }
