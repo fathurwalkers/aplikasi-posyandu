d35d9a2ead8e32ee24dbf07fc8d67298d6048966
Fathur Walkers <fathurwalkers44@gmail.com>
2022-03-19 06:00:32 -0700

    update

--- app/Http/Controllers/AnakController.php
+++ app/Http/Controllers/AnakController.php
@@ -38,14 +38,31 @@ class AnakController extends Controller
         if ($data == null) {
             return redirect()->route('data-anak')->with('status', 'Data yang anda ingin ubah tidak dapat ditemukan. ');
         } else {
+            $gambar_cek = $request->file('data_foto');
+            if (!$gambar_cek) {
+                $gambar_ori = $data->data_foto;
+                $gambar = $gambar_ori;
+            } else {
+                $ext = $request->file('data_foto')->getClientOriginalExtension();
+                $randomNamaGambar = strtolower(Str::random(10)) . "." .$ext;
+                $gambar_ori = $request->file('data_foto')->move(public_path('default-img/foto'), $randomNamaGambar);
+                $gambar = $randomNamaGambar;
+            }
+            $umur = intval($request->data_umur);
+            if ($request->data_umur < 38) {
+                $tipe                   = "BALITA";
+            } else {
+                $tipe                   = "ANAK";
+            }
+
             $save_update_data = $data->update([
-                "data_foto"             => $request->data_foto,
+                "data_foto"             => $gambar,
                 "data_nama_lengkap"     => $request->data_nama_lengkap,
                 "data_nama_orang_tua"   => $request->data_nama_orang_tua,
                 "data_alamat_lengkap"   => $request->data_alamat_lengkap,
                 "data_jenis_kelamin"    => $request->data_jenis_kelamin,
-                "data_tipe"             => $request->data_tipe,
-                "data_umur"             => intval($request->data_umur),
+                "data_tipe"             => $tipe,
+                "data_umur"             => $umur,
                 "updated_at"            => now()
             ]);
             return redirect()->route('admin-data-anak')->with('status', 'Data dengan nama "' . $data->data_nama_lengkap . '" Telah berhasil diubah!');
