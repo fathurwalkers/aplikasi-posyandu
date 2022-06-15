25849c3993595e86b76b7f4c0193185eec74c3c3
Fathur Walkers <fathurwalkers44@gmail.com>
2022-06-15 14:45:42 -0700

    update

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -33,95 +33,97 @@
             </div>
 
             @if ($users->data_id == null)
-            <a href="{{ route('pengisian-data') }}"
-                class="card mb-3 text-decoration-none status-gizi text-white p-0">
-                <div class="row g-0">
-                    <div class="col-8">
-                        <div class="card-body px-1">
-                            <h5 class="card-title p-0 m-0">Pengisian Data</h5>
-                            <p class="card-text p-0 m-0">Pengisian Biodata Pengguna</p>
+                <a href="{{ route('pengisian-data') }}"
+                    class="card mb-3 text-decoration-none status-gizi text-white p-0">
+                    <div class="row g-0">
+                        <div class="col-8">
+                            <div class="card-body px-1">
+                                <h5 class="card-title p-0 m-0">Pengisian Data</h5>
+                                <p class="card-text p-0 m-0">Pengisian Biodata Pengguna</p>
+                            </div>
+                        </div>
+                        <div class="col-4 mx-auto my-auto text-center py-4">
+                            <i style="font-size: 45px" class="fa fa-list-alt p-0"></i>
                         </div>
                     </div>
-                    <div class="col-4 mx-auto my-auto text-center py-4">
-                        <i style="font-size: 45px" class="fa fa-list-alt p-0"></i>
-                    </div>
-                </div>
-            </a>
-            @else
-            @if ($users->login_level == 'admin')
-            <a href="{{ route('menu-admin') }}"
-                class="card mb-3 text-decoration-none status-gizi text-white p-0">
-                <div class="row g-0">
-                    <div class="col-8">
-                        <div class="card-body px-1">
-                            <h5 class="card-title p-0 m-0">Administrator Menu</h5>
-                            <p class="card-text p-0 m-0">Daftar Menu untuk administrator</p>
+                </a>
+                @else
+                @if ($users->login_level == 'admin')
+                <a href="{{ route('menu-admin') }}"
+                    class="card mb-3 text-decoration-none status-gizi text-white p-0">
+                    <div class="row g-0">
+                        <div class="col-8">
+                            <div class="card-body px-1">
+                                <h5 class="card-title p-0 m-0">Administrator Menu</h5>
+                                <p class="card-text p-0 m-0">Daftar Menu untuk administrator</p>
+                            </div>
+                        </div>
+                        <div class="col-4 mx-auto my-auto text-center py-4">
+                            <i style="font-size: 45px" class="fa fa-list-alt p-0"></i>
                         </div>
                     </div>
-                    <div class="col-4 mx-auto my-auto text-center py-4">
-                        <i style="font-size: 45px" class="fa fa-list-alt p-0"></i>
-                    </div>
-                </div>
-            </a>
+                </a>
 
-            @endif
+                @endif
 
-            <a href="{{ route('client-menu-makanan') }}"
-                class="card kartu mb-3 text-decoration-none bg-warning text-white p-0">
-                <div class="row g-0">
-                    <div class="col-8">
-                        <div class="card-body px-1">
-                            <h5 class="card-title p-0 m-0">Daftar Makanan</h5>
-                            <p class="card-text p-0 m-0">Berisi tentang rekomendasi makanan sehat bagi anak</p>
+                <a href="{{ route('client-menu-makanan') }}"
+                    class="card kartu mb-3 text-decoration-none bg-warning text-white p-0">
+                    <div class="row g-0">
+                        <div class="col-8">
+                            <div class="card-body px-1">
+                                <h5 class="card-title p-0 m-0">Daftar Makanan</h5>
+                                <p class="card-text p-0 m-0">Berisi tentang rekomendasi makanan sehat bagi anak</p>
+                            </div>
+                        </div>
+                        <div class="col-4 mx-auto my-auto text-center py-4">
+                            <i style="font-size: 45px" class="fa fa-list p-0"></i>
                         </div>
                     </div>
-                    <div class="col-4 mx-auto my-auto text-center py-4">
-                        <i style="font-size: 45px" class="fa fa-list p-0"></i>
-                    </div>
-                </div>
-            </a>
-            <a href="{{ route('client-menu-hitung-gizi') }}"
+                </a>
+                @if ($users->login_level == 'pengguna')
+                <a href="{{ route('client-menu-hitung-gizi') }}"
                 class="card mb-3 text-decoration-none hitung-gizi text-white p-0">
-                <div class="row g-0">
-                    <div class="col-8">
-                        <div class="card-body px-1">
-                            <h5 class="card-title p-0 m-0">Hitung Gizi</h5>
-                            <p class="card-text p-0 m-0">Menghitung status gizi anak</p>
+                    <div class="row g-0">
+                        <div class="col-8">
+                            <div class="card-body px-1">
+                                <h5 class="card-title p-0 m-0">Hitung Gizi</h5>
+                                <p class="card-text p-0 m-0">Menghitung status gizi anak</p>
+                            </div>
+                        </div>
+                        <div class="col-4 mx-auto my-auto text-center py-4">
+                            <i style="font-size: 45px" class="fa fa-calculator p-0"></i>
                         </div>
                     </div>
-                    <div class="col-4 mx-auto my-auto text-center py-4">
-                        <i style="font-size: 45px" class="fa fa-calculator p-0"></i>
-                    </div>
-                </div>
-            </a>
-            <a href="{{ route('menu-grafik') }}"
-                class="card mb-3 text-decoration-none grafik text-white p-0">
-                <div class="row g-0">
-                    <div class="col-8">
-                        <div class="card-body px-1">
-                            <h5 class="card-title p-0 m-0">Grafik Pertumbuhan</h5>
-                            <p class="card-text p-0 m-0">Memonitoring gizi pertumbuhan anak setiap bulan</p>
+                </a>
+                <a href="{{ route('menu-grafik') }}"
+                    class="card mb-3 text-decoration-none grafik text-white p-0">
+                    <div class="row g-0">
+                        <div class="col-8">
+                            <div class="card-body px-1">
+                                <h5 class="card-title p-0 m-0">Grafik Pertumbuhan</h5>
+                                <p class="card-text p-0 m-0">Memonitoring gizi pertumbuhan anak setiap bulan</p>
+                            </div>
+                        </div>
+                        <div class="col-4 mx-auto my-auto text-center py-4">
+                            <i style="font-size: 45px" class="fa fa-chart-line p-0"></i>
                         </div>
                     </div>
-                    <div class="col-4 mx-auto my-auto text-center py-4">
-                        <i style="font-size: 45px" class="fa fa-chart-line p-0"></i>
-                    </div>
-                </div>
-            </a>
-            <a href="{{ route('status-gizi') }}"
-                class="card mb-3 text-decoration-none status-gizi text-white p-0">
-                <div class="row g-0">
-                    <div class="col-8">
-                        <div class="card-body px-1">
-                            <h5 class="card-title p-0 m-0">Status Gizi</h5>
-                            <p class="card-text p-0 m-0">Untuk melihat status gizi anak</p>
+                </a>
+                <a href="{{ route('status-gizi') }}"
+                    class="card mb-3 text-decoration-none status-gizi text-white p-0">
+                    <div class="row g-0">
+                        <div class="col-8">
+                            <div class="card-body px-1">
+                                <h5 class="card-title p-0 m-0">Status Gizi</h5>
+                                <p class="card-text p-0 m-0">Untuk melihat status gizi anak</p>
+                            </div>
+                        </div>
+                        <div class="col-4 mx-auto my-auto text-center py-4">
+                            <i style="font-size: 45px" class="fa fa-list-alt p-0"></i>
                         </div>
                     </div>
-                    <div class="col-4 mx-auto my-auto text-center py-4">
-                        <i style="font-size: 45px" class="fa fa-list-alt p-0"></i>
-                    </div>
-                </div>
-            </a>
+                </a>
+                @endif
             @endif
 
         </div>
